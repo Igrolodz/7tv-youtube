172e01e4c8ad1e923dfe93ca538c39f28f736629
Igrolodz <igorkolodz@gmail.com>
2025-01-01 19:09:57 +0100

    Demo

--- content.js
+++ content.js
@@ -1,15 +1,38 @@
-// Function to replace keywords with images
+/* 
+    We probably want to get those values from 7tv database and set width dynamically from emote ratio.
+    It's just a concept I have no idea how you handle emotes i.e. Twitch.
+*/
+const shortEmote = 'WHAT';
+const shortWidth = "16px";
+const shortEmoteRegex = new RegExp(`(?<!alt="[^"]*)\\b${shortEmote}\\b`, 'g'); // Regex to find the word "WHAT" (case-sensitive) and exclude it from alt attribute
+
+const longEmote = 'om';
+const longWidth = "72px";
+const longEmoteRegex = new RegExp(`(?<!alt="[^"]*)\\b${longEmote}\\b`, 'g'); // Regex to find the word "om" (case-sensitive) and exclude it from alt attribute
+
+// Function to replace keywords with emotes
 function replaceKeywords() {
     const comments = document.querySelectorAll("span.yt-core-attributed-string.yt-core-attributed-string--white-space-pre-wrap");
 
     comments.forEach((comment) => {
         // Check if the keyword exists in the comment
         const originalText = comment.innerHTML;
-        const updatedText = originalText.replace(
-            /(?<!alt="[^"]*)\bWHAT\b/g, // Regex to find the word "WHAT" (case-sensitive)
-            `<img src="https://cdn.7tv.app/emote/01G4ZTECKR0002P97QQ94BDSP4/2x.avif" alt="WHAT Emote" style="width:16px; height:16px;">`
+        var updatedText = originalText.replace(
+            shortEmoteRegex,
+            `<img src="https://cdn.7tv.app/emote/01G4ZTECKR0002P97QQ94BDSP4/4x.avif" alt="${shortEmote}" style="width:${shortWidth}; height:16px;">` //once again, get src from database
         );
         // Update the comment's HTML if any replacement occurred
+        if (originalText !== updatedText) {
+            comment.innerHTML = updatedText;
+        }
+
+        // Preferably we would merge both together into one foreach loop checking for every channel emote instead manually stacking updatedText and ifs
+
+        updatedText = originalText.replace(
+            longEmoteRegex,
+            `<img src="https://cdn.7tv.app/emote/01FET023A8000C9XXF2H0G0AQF/4x.avif" alt="${longEmote}" style="width:${longWidth}; height:16px;">`
+        );
+
         if (originalText !== updatedText) {
             comment.innerHTML = updatedText;
         }
@@ -32,22 +55,22 @@ const observer = new MutationObserver(callback);
 
 // Start observing the comment section once it's available
 function startObserving() {
-    const commentSection = document.querySelector("ytd-item-section-renderer#sections");
+    var commentSection = document.querySelector(`ytd-item-section-renderer#sections`);
     if (!commentSection) {
-        console.log("can't find comment section");
+        console.log("comment section not found");
         setTimeout(startObserving, 1000);
         return;
     }
 
-    const contentDiv = commentSection.querySelector("div#contents");
+    var contentDiv = commentSection.querySelector("div#contents");
     if (!contentDiv) {
-        console.log("can't find content div");
+        console.log("contents not found");
         setTimeout(startObserving, 1000);
         return;
     }
 
     console.log("Everything found, starting observing...");
-    observer.observe(document.body, { childList: true, subtree: true });
+    observer.observe(document.body, { childList: true, subtree: true, }); // Temporarily document.body as a target. I have no idea why code above suddenly stopped working
 }
 
 // Initial start
